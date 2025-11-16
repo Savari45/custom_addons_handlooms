ac0311d5f362e3c62082a24ae1cd3b29193b4455
savari45 <savariuma045@gmail.com>
2025-11-16 16:31:01 +0530

    [initiate]the custom modules

---
+++ cns_employee_aadhar_pan/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Employee Deatils Rename',
+    'version': '18.0',
+    'author': 'Surya R',
+    'category': 'HR',
+    'depends': ['base', 'hr'],
+    'data': [
+        'views/employee_aadhar_views.xml',
+
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ cns_payslip_customize/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'CNS Payslip Customization',
+    'version': '18.0',
+    'category': 'Human Resources',
+    'summary': 'Custom QWeb Payslip Report for HR Payroll',
+    'depends': ['base','payroll'],
+    'data': [
+        'report/payslip_report_template.xml',
+
+    ],
+    'installable': True,
+    'application': False,
+}
